36a9dc7142319d0878978c953551af9fdce69c7a
sizo2007 <sicocxlesizo69@gmail.com>
2025-04-24 07:40:42 -0700

    init

---
+++ Assets/script/zuka1.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class zuka1 : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Start");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Update");
+    }
+}
